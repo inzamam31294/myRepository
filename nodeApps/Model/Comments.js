87ce74b9f63c26f28901cdeb3f596ca5fdb6f32c
dev <inzamam54321@gmail.com>
2019-04-12 20:13:57 +0500

    My new commit

--- nodeApps/Model/Comments.js
+++ nodeApps/Model/Comments.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const comments_schema = new mongoose.Schema({
-    content: {
-        type: String,
-        required: "Content is required"
-    },
-   post: {
-       type: mongoose.Schema.Types.ObjectId,
-       ref: "Post",
-       required: "Post is required Field"
-    }
+const commentsSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: 'Content is required',
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    required: 'Post is required Field',
+  },
 });
 
-module.exports = mongoose.model("Comments", comments_schema);
+module.exports = mongoose.model('Comments', commentsSchema);
--- nodeApps/Model/Post.js
+++ nodeApps/Model/Post.js
@@ -1,25 +1,25 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const post_schema = new mongoose.Schema({
+const postSchema = new mongoose.Schema({
 
-    title: {
-        type: String,
-        required: "Title is required"
-    },
-    content: {
+  title: {
+    type: String,
+    required: 'Title is required',
+  },
+  content: {
 
-        type: String,
-        required: "Content is required"
-    },
-    comments: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Comments",
-        required: "Comment is required"
-    }]
+    type: String,
+    required: 'Content is required',
+  },
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comments',
+    required: 'Comment is required',
+  }],
 },
 {
-    timestamps: true
+  timestamps: true,
 }
 );
 
-module.exports = mongoose.model("Post", post_schema);
+module.exports = mongoose.model('Post', postSchema);
