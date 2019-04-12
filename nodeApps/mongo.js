f5f4379f0ba72ff85728ede47388115685875283
dev <inzamam54321@gmail.com>
2019-04-12 20:00:40 +0500

    new commit

--- nodeApps/mongo.js
+++ nodeApps/mongo.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-const mongoDBErrors = require("mongoose-mongodb-errors");
+const mongoose = require('mongoose');
+require('dotenv').config();
+const mongoDBErrors = require('mongoose-mongodb-errors');
 
 mongoose.Promise = global.Promise;
 mongoose.plugin(mongoDBErrors);
--- nodeApps/server.js
+++ nodeApps/server.js
@@ -1,48 +1,47 @@
-//const http = require("http");
-const express = require("express");
+// const http = require("http");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const bodyparser = require("body-parser");
-const morgan = require("morgan");
-require("express-async-errors");
+require('mongoose');
+const bodyparser = require('body-parser');
+const morgan = require('morgan');
+require('express-async-errors');
 
-//database connection
-require("./mongo");
+// database connection
+require('./mongo');
 
-//Models
-require("./Model/Post");
-require("./Model/Comments");
+// Models
+require('./Model/Post');
+require('./Model/Comments');
 
-//Middleware
+// Middleware
 app.use(bodyparser.json()).use(morgan());
 
-//Routes
-app.use("/posts", require("./Routes/posts"))
+// Routes
+app.use('/posts', require('./Routes/posts'));
 
-//Route not found
-app.use(function(req, res, next){
-    req.status = 404;
-    const error = new Error("Routes not found");
-    next(error);
-})
-
-if(app.get("env") === "production") {
-    app.use(function(error, req, res, next){
-        res.status(req.status || 500).send({
-            message: error.message,
-        });
+// Route not found
+app.use(function(req, res, next) {
+  req.status = 404;
+  const error = new Error('Routes not found');
+  next(error);
 });
-}
-//error handler
-app.use( function(error, req, res, next){
+
+if (app.get('env') === 'production') {
+  app.use(function(error, req, res, next) {
     res.status(req.status || 500).send({
-        message: error.message,
-        stack: error.stack
+      message: error.message,
     });
+  });
+}
+// error handler
+app.use( function(error, req, res, next) {
+  res.status(req.status || 500).send({
+    message: error.message,
+    stack: error.stack,
+  });
 });
 
 
-app.listen(7272, function(){
-    
-    console.log("server is running on port 7272");   
-})
+app.listen(7272, function() {
+  console.log('server is running on port 7272');
+});
